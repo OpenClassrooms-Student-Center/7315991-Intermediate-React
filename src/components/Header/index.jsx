73bb92280ed6c13ceab13c36e71c2785f0f7e0ef
Max Mahmoud Wardeh <m@lurnid.com>
2021-11-14 19:55:43 +0000

    Update translations

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 function Header() {
   return (
     <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/survey/1">Questionnaire</Link>
+      <Link to="/">Home</Link>
+      <Link to="/survey/1">Survey</Link>
     </nav>
   )
 }
